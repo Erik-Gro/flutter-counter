5eb7d672f9621b89ca1f0f7cc517ab379d7734b3
Cirrus CI <support@cirruslabs.org>
2026-04-07 20:08:14 +0000

    add counter

--- lib/main.dart
+++ lib/main.dart
@@ -7,116 +7,183 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Advanced Counter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CounterPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CounterPage> createState() => _CounterPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CounterPageState extends State<CounterPage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _modifyCounter(int amount) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter += amount;
+    });
+  }
+
+  void _multiplyCounter() {
+    setState(() {
+      _counter *= 2;
+    });
+  }
+
+  void _halveCounter() {
+    setState(() {
+      _counter ~/= 2;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Candy Counter'),
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      // ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Current Counter Value:',
+              style: TextStyle(fontSize: 20),
+            ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: const TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(height: 40),
+
+            _buildAdditionSubtractionRows(),
+            const SizedBox(height: 20),
+            _buildMultiplicationDivisionRow(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _buildAdditionSubtractionRows() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomActionBtn(
+              label: '+1',
+              color: Colors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              onPressed: () => _modifyCounter(1),
+            ),
+            const SizedBox(width: 10),
+            CustomActionBtn(
+              label: '-1',
+              color: Colors.red,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              onPressed: () => _modifyCounter(-1),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomActionBtn(
+              label: '+2',
+              color: Colors.lightGreen,
+              shape: const StadiumBorder(),
+              onPressed: () => _modifyCounter(2),
+            ),
+            const SizedBox(width: 10),
+            CustomActionBtn(
+              label: '-2',
+              color: Colors.orange,
+              shape: const StadiumBorder(),
+              onPressed: () => _modifyCounter(-2),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMultiplicationDivisionRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomActionBtn(
+          label: '* 2',
+          color: Colors.purple,
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(20),
+          onPressed: _multiplyCounter,
+        ),
+        const SizedBox(width: 10),
+        CustomActionBtn(
+          label: '/ 2',
+          color: Colors.blue,
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(20),
+          onPressed: _halveCounter,
+        ),
+      ],
+    );
+  }
+}
+
+class CustomActionBtn extends StatelessWidget {
+  final String label;
+  final Color color;
+  final VoidCallback onPressed;
+  final OutlinedBorder shape;
+  final EdgeInsetsGeometry? padding;
+
+  const CustomActionBtn({
+    super.key,
+    required this.label,
+    required this.color,
+    required this.onPressed,
+    required this.shape,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        foregroundColor: Colors.white,
+        shape: shape,
+        padding:
+            padding ?? const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
       ),
     );
   }
 }
+
+
+//flutter run -d web-server --web-port=8080 --web-hostname=0.0.0.0
